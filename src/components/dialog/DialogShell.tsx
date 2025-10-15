aea50848984948a5d0a304b8e3bdf975a72c9a0a
Richard <richardcreedon1989@gmail.com>
2025-10-15 17:11:14 +0100

    Dialog

--- src/components/dialog/DialogShell.tsx
+++ src/components/dialog/DialogShell.tsx
@@ -3,7 +3,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
-
+import React from "react";
 export default function DialogShell({
   open,
   title,
